4679a62eeeaf9ce27c0e9d764a730723e5493407
jiins8 <117478765+jiins8@users.noreply.github.com>
2025-10-06 19:22:16 +0200

    home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Home],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })

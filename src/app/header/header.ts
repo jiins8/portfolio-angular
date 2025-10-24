c59a4356e2a0f714eb23bcf793eba51c8d4c9135
jiins8 <117478765+jiins8@users.noreply.github.com>
2025-10-24 19:33:54 +0200

    header buttons improved

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -11,19 +11,23 @@ export class Header {
   activeSection = 'home';
   isDarkMode = false;
 
-  
-toggleMenu() {
-  const icon = document.querySelector('.menu-icon') as HTMLElement;
-  if (icon) {
-    icon.classList.add('ng-animating'); // activa animación
-    setTimeout(() => {
-      this.menuActive = !this.menuActive; // cambia el icono
-      icon.classList.remove('ng-animating'); // termina animación
-    }, 150); // tiempo de la animación
-  } else {
-    this.menuActive = !this.menuActive;
+  toggleMenu() {
+    const icon = document.querySelector('.menu-icon') as HTMLElement;
+    if (icon) {
+      icon.classList.add('ng-animating');
+      setTimeout(() => {
+        this.menuActive = !this.menuActive;
+        icon.classList.remove('ng-animating');
+      }, 150);
+    } else {
+      this.menuActive = !this.menuActive;
+    }
   }
-}
+
+  /** ✅ Nuevo método: cierra el menú (sin animación inversa del icono) */
+  closeMenu() {
+    this.menuActive = false;
+  }
 
   toggleDarkMode() {
     this.isDarkMode = !this.isDarkMode;
@@ -49,6 +53,4 @@ toggleMenu() {
 
     this.activeSection = current;
   }
-
-  
 }
